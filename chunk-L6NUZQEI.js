e9dec29b18651f73ebc0590e631eb46b189a08fa
rollthecloudinc <>
2026-01-21 11:50:32 +0000

    Update from https://github.com/rollthecloudinc/spearhead/commit/814781997d402176e860d57904154a268ccb87eb

--- chunk-L6NUZQEI.js
+++ chunk-L6NUZQEI.js
@@ -25636,7 +25636,7 @@ var panelpages2 = [
         "data": {
           "exposedModule": "./TabulatorModule",
           "moduleName": "TabulatorModule",
-          "remoteEntry": "https://solid-tabulator.rollthecloud.com/20.0.0-alpha.44/remoteEntry.json"
+          "remoteEntry": "https://solid-tabulator.rollthecloud.com/20.0.0-alpha.48/remoteEntry.json"
         },
         "name": "plugin-tabulator",
         "plugin": "module"
@@ -28189,4 +28189,4 @@ var AppModule = class _AppModule {
 
 // projects/spear/src/bootstrap.ts
 __NgCli_bootstrap_1.platformBrowser().bootstrapModule(AppModule).catch((err) => console.error(err));
-//# sourceMappingURL=chunk-MMRIHFHE.js.map
+//# sourceMappingURL=chunk-L6NUZQEI.js.map
--- main-3TVEGDTK.js
+++ main-3TVEGDTK.js
@@ -179,6 +179,6 @@ function processHostInfo(hostInfo, relBundlesPath = "./") {
 
 // projects/spear/src/main.ts
 initFederation().catch((err) => console.error("Error loading federation configuration:", err)).then((_) => {
-  import("./chunk-MMRIHFHE.js").catch((err) => console.error(err));
+  import("./chunk-L6NUZQEI.js").catch((err) => console.error(err));
 });
-//# sourceMappingURL=main-LAOIAHVT.js.map
+//# sourceMappingURL=main-3TVEGDTK.js.map
