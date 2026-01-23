334495142e56d7274a95d991e64013b08b8efd4c
rollthecloudinc <>
2026-01-23 18:02:20 +0000

    Update from https://github.com/rollthecloudinc/spearhead/commit/814781997d402176e860d57904154a268ccb87eb

--- main-IYBMHZ4H.js
+++ main-IYBMHZ4H.js
@@ -179,6 +179,6 @@ function processHostInfo(hostInfo, relBundlesPath = "./") {
 
 // projects/spear/src/main.ts
 initFederation().catch((err) => console.error("Error loading federation configuration:", err)).then((_) => {
-  import("./chunk-MM3LT75S.js").catch((err) => console.error(err));
+  import("./chunk-VE2SMBA5.js").catch((err) => console.error(err));
 });
-//# sourceMappingURL=main-PNHGNTDF.js.map
+//# sourceMappingURL=main-IYBMHZ4H.js.map
