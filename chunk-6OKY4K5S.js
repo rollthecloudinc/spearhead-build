3f325a1fd5cb769a3a33cbd83e635e9ee18a2fcd
rollthecloudinc <>
2026-01-29 05:23:13 +0000

    Update from https://github.com/rollthecloudinc/spearhead/commit/570227b87b8716375143acc2b6c90759df73ac94

--- chunk-6OKY4K5S.js
+++ chunk-6OKY4K5S.js
@@ -3653,11 +3653,11 @@ var panelpages2 = [
             "settings": [
               {
                 "attributes": [],
-                "computedValue": "[form__test-listbox-int.size]",
+                "computedValue": "[form__test-listbox-int.size[*]]",
                 "displayName": "value",
                 "name": "value",
                 "type": 1,
-                "value": "[form__test-listbox-int.size]"
+                "value": "[form__test-listbox-int.size[*]]"
               },
               {
                 "attributes": [
@@ -30760,4 +30760,4 @@ var AppModule = class _AppModule {
 
 // projects/spear/src/bootstrap.ts
 __NgCli_bootstrap_1.platformBrowser().bootstrapModule(AppModule).catch((err) => console.error(err));
-//# sourceMappingURL=chunk-43WX7HLN.js.map
+//# sourceMappingURL=chunk-6OKY4K5S.js.map
